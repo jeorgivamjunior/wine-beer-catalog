3084e293b81d0f06b39a9d48edf0bad8a6b73fcb
Jeorgivam Junior <JeorgivamJunior@gmail.com>
2022-09-15 20:38:23 +0100

    Add router library

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { FC } from 'react';
 
+import { RouterProvider } from 'react-router-dom';
+
 import { Header } from './components';
 import { AppContextProvider } from './contexts';
+import { router } from './routes';
 
 export const App: FC = () => {
   return (
     <AppContextProvider>
       <Header />
-      <Routes />
+      <RouterProvider router={router} />
     </AppContextProvider>
   );
 };
