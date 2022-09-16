b026d0886a1e5ce7bdb69a42d8ce2165c933e716
Jeorgivam Junior <JeorgivamJunior@gmail.com>
2022-09-17 00:06:16 +0100

    Update readme

--- src/pages/Products/components/FilterBar/FilterBar.tsx
+++ src/pages/Products/components/FilterBar/FilterBar.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 
-import { Grid, TextField, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { Grid, TextField, FormControl, InputLabel, Select, MenuItem, Tooltip } from '@mui/material';
 import { Box } from '@mui/system';
 
 export const FilterBar: FC = () => {
@@ -8,23 +8,27 @@ export const FilterBar: FC = () => {
     <Box marginBottom={5} marginTop={10}>
       <Grid container spacing={2} justifyContent="space-between">
         <Grid>
-          <TextField
-            label="Search field"
-            type="search"
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
+          <Tooltip title="Not implemented">
+            <TextField
+              label="Search field"
+              type="search"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Tooltip>
         </Grid>
         <Grid>
-          <FormControl fullWidth>
-            <InputLabel>Sort</InputLabel>
-            <Select value={1} label="Sort">
-              <MenuItem value={1}>By popularity</MenuItem>
-              <MenuItem value={2}>By price: low to high</MenuItem>
-              <MenuItem value={3}>By price: high to low</MenuItem>
-            </Select>
-          </FormControl>
+          <Tooltip title="Not implemented">
+            <FormControl fullWidth>
+              <InputLabel>Sort</InputLabel>
+              <Select value={1} label="Sort">
+                <MenuItem value={1}>By popularity</MenuItem>
+                <MenuItem value={2}>By price: low to high</MenuItem>
+                <MenuItem value={3}>By price: high to low</MenuItem>
+              </Select>
+            </FormControl>
+          </Tooltip>
         </Grid>
       </Grid>
     </Box>
