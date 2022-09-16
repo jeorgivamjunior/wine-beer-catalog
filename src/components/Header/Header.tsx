cc3e550fc363525532b7ccc21890e930bbbe3460
Jeorgivam Junior <JeorgivamJunior@gmail.com>
2022-09-16 11:36:05 +0100

    Adjust header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,15 @@
 import { FC } from 'react';
 
+import { AppBar, Toolbar, Typography } from '@mui/material';
+
 export const Header: FC = () => {
-  return <header>Wine/Beer Finder</header>;
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Wine and Beer Finder
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 };
